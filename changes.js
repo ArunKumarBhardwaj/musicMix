383fdbcff60f3b1ae57e942d4cf6a2503fa0734d
ArunKumarBhardwaj <bhardwajarun38@gmail.com>
2020-09-14 22:42:36 +0530

    initialized project

---
+++ changes.js
@@ -0,0 +1,37 @@
+
+    function music(id){
+      
+      if(id === "d"){
+        let audio = new Audio("Big_Thwack_120.mp3")
+        audio.play()
+      }
+      else if(id==="p"){
+          let audio = new Audio("J_Syreus_Bach_-_Lesser_Faith.mp3")
+          alert("hi")
+          audio.play()
+        }
+      }
+
+
+    
+
+
+
+    // function piano(id){
+    //   var audio = new Audio("J_Syreus_Bach_-_Lesser_Faith")
+    //   audio.play();
+    // }
+    
+    
+    // function guitar(id){
+    //   var audio = new Audio("Lorenzos_Music_-_03_-_Guitar_strut_1111")
+    //   audio.play();
+    // }
+    
+    
+    // function bass(id){
+    //   var audio = new Audio("AlzAlya_DrOwL_-_02_-_Basscadet_dream_version")
+    //   audio.play();
+    // }
+    
+  
